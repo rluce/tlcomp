532d4f3aded7154fbfc987849330f9d52b44b11a
Robert Luce <rbobluce@gmail.com>
2017-03-04 20:33:21 +0100

    Minor extensions

--- ToepMatStein.m
+++ ToepMatStein.m
@@ -9,8 +9,44 @@ classdef ToepMatStein
             % T = ToepMatStein(c,r)
             %   T is Toeplitz matrix with prescribed first col/row
             
+            
+            if min(size(c)) > 1 || min(size(r)) > 1
+                error('funmd:InconsistentInput', 'c, r must be vectors');
+            end
+            
+            % Homogenize
+            c = c(:);
+            r = r(:);
+            
+            if length(c) ~= length(r)
+                error('funmd:InconsistentInput', 'c, r must have same length');
+            end
+            
+            n = length(r);
+            
+            if n>0 && c(1) ~= r(1)
+                error('funmd:InconsistentInput', 'c(1),r(1) must be equal');
+            end
+            
             T.c = c;
             T.r = r;
+        end
+       
+        function [s1, s2] = size(T)
+            s1 = length(T.c);
+            s2 = length(T.r);
+            if nargout < 2
+                s1 = [s1, s2];
+            end
+        end
+        
+        function TF = full(T)
+            if isempty(T.c)
+                TF = [];
+                return;
+            end
+            
+            TF = toeplitz(T.c, T.r);
         end
         
     end
--- test/ToepMatSteinTest.m
+++ test/ToepMatSteinTest.m
@@ -18,7 +18,68 @@ testCase.assertEqual(T.r, -1);
 [c,r] = random_toeplitz(7,7);
 
 T = ToepMatStein(c,r);
-testCase.assertEqual(T.c, c);
-testCase.assertEqual(T.r, r);
+testCase.assertEqual(T.c, c(:));
+testCase.assertEqual(T.r, r(:));
+
+c = 6;
+r = 1;
+testCase.assertError( @() ToepMatStein(c,r), 'funmd:InconsistentInput');
+
+c = rand(9,1);
+r = c;
+r(1) = r(1) + 1e-8;
+testCase.assertError( @() ToepMatStein(c,r), 'funmd:InconsistentInput');
+
+% For the moment, we only support square matrices
+c = ones(4,1);
+r = ones(3,1);
+testCase.assertError( @() ToepMatStein(c,r), 'funmd:InconsistentInput');
+
+% Only vectors allowed
+c = ones(4,2);
+r = ones(4,2);
+testCase.assertError( @() ToepMatStein(c,r), 'funmd:InconsistentInput');
 
 end
+
+function test_size(testCase)
+
+T = ToepMatStein([], []);
+s = size(T);
+testCase.assertEqual(s, [0,0]);
+[s1, s2] = size(T);
+testCase.assertEqual(s1, 0);
+testCase.assertEqual(s2, 0);
+
+T = ToepMatStein(1,1);
+s = size(T);
+testCase.assertEqual(s, [1,1]);
+[s1, s2] = size(T);
+testCase.assertEqual(s1, 1);
+testCase.assertEqual(s2, 1);
+
+T = ToepMatStein([1,2,3], [1,0, -1]);
+s = size(T);
+testCase.assertEqual(s, [3,3]);
+[s1, s2] = size(T);
+testCase.assertEqual(s1, 3);
+testCase.assertEqual(s2, 3);
+
+
+end
+
+function test_full(testCase)
+T = ToepMatStein([],[]);
+testCase.assertEqual(full(T), []);
+
+T = ToepMatStein(-1,-1);
+testCase.assertEqual(full(T), -1);
+
+T = ToepMatStein([1,0,0], [1,0,0]);
+testCase.assertEqual(full(T), eye(3));
+
+[c,r,T] = random_toeplitz(8,8);
+TM = ToepMatStein(c,r);
+testCase.assertEqual(full(TM), T);
+end
+
