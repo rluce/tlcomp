ef4153a8a7a6bb2ab8f221d5371322d6e56bde81
Robert Luce <rbobluce@gmail.com>
2017-09-28 20:32:07 +0200

    Refactor

---
+++ matlab/test/toepeyeTest.m
@@ -0,0 +1,21 @@
+function tests = toepeyeTest
+
+tests = functiontests(localfunctions);
+
+end
+
+function test_teye(testCase)
+
+E = toepeye(0);
+testCase.assertEqual(class(E), 'ToepMat');
+testCase.assertTrue(isempty(full(E)));
+
+E = toepeye(1);
+testCase.assertEqual(class(E), 'ToepMat');
+testCase.assertEqual(full(E),1);
+
+E = toepeye(9);
+testCase.assertEqual(class(E), 'ToepMat');
+testCase.assertEqual(full(E),eye(9));
+
+end
--- matlab/test/utilTest.m
+++ matlab/test/utilTest.m
@@ -5,21 +5,6 @@ tests = functiontests(localfunctions);
 
 end
 
-function test_teye(testCase)
-
-E = toepeye(0);
-testCase.assertEqual(class(E), 'ToepMat');
-testCase.assertTrue(isempty(full(E)));
-
-E = toepeye(1);
-testCase.assertEqual(class(E), 'ToepMat');
-testCase.assertEqual(full(E),1);
-
-E = toepeye(9);
-testCase.assertEqual(class(E), 'ToepMat');
-testCase.assertEqual(full(E),eye(9));
-
-end
 
 function test_is_exact_toeplitz(testCase)
 testCase.assertTrue(is_exact_toeplitz([]));
