2dfd87572ab52c8acd19e5df325914b5348e1ea3
Robert Luce <rbobluce@gmail.com>
2017-10-02 11:51:51 +0200

    Refactor and parameterize test

--- matlab/test/toeplksquareTest.m
+++ matlab/test/toeplksquareTest.m
@@ -1,72 +1,81 @@
-function tests = toeplksquareTest
+classdef toeplksquareTest < matlab.unittest.TestCase
+    properties (TestParameter)
+        alg = {'full', 'fft'};
+    end
+    
+    methods (Test)
+        
+        function test_singleton(testCase, alg)
+            % 1x1 zero
+            G = 0;
+            B = 0;
+            [Gs, Bs] = toeplksquare(G, B);
+            T2 = toeplkreconstruct(Gs, Bs);
+            testCase.assertEqual(T2, 0);
+            
+            % 1x1 identity
+            G = 2;
+            B = 1;
+            Ts_true = 1.0;
+            
+            [Gs, Bs] = toeplksquare(G, B, alg);
+            Ts = toeplkreconstruct(Gs, Bs);
+            testCase.assertEqual(Ts, Ts_true, 'AbsTol', eps);
+        end
+        
+        function test_identity(testCase, alg)
+            n=15;
+            e1 = zeros(n, 1);
+            e1(1) = 1.0;
+            [G, B] = toepgen(e1, e1);
+            Ts_true = eye(n);
 
-tests = functiontests(localfunctions);
+            [Gs, Bs] = toeplksquare(G, B, alg);
+            Ts = toeplkreconstruct(Gs, Bs);
+            testCase.assertEqual(Ts, Ts_true, 'AbsTol', n*eps);
+        end
+        
+        function test_random_real(testCase, alg)
+            n = 12;
+            c = randn(n,1);
+            r = randn(n,1);
+            r(1) = c(1);
+            T = toeplitz(c,r);
+            Ts_true = T*T;
+            [G, B] = toepgen(c,r);
 
+            [Gs, Bs] = toeplksquare(G, B, alg);
+            Ts = toeplkreconstruct(Gs, Bs);
+            testCase.assertEqual(Ts, Ts_true, 'AbsTol', n*eps);
+        end
+        
+        
+        function test_random_complex(testCase, alg)
+            n = 12;
+            c = randn(n,1) + 1i*randn(n,1);
+            r = randn(n,1) + 1i*randn(n,1);
+            r(1) = c(1);
+            T = toeplitz(c,r);
+            Ts_true = T*T;
+            [G, B] = toepgen(c,r);
+            [Gs, Bs] = toeplksquare(G, B, alg);
+            Ts = toeplkreconstruct(Gs, Bs);
+            testCase.assertEqual(Ts, Ts_true, 'AbsTol', n*eps);
+        end
+        
+        function test_random_d5(testCase, alg)
+            n = 16;
+            d = 5;
+            G = randn(n,d) + 1i * randn(n,d);
+            B = randn(n,d) + 1i * randn(n,d);
+            T = toeplkreconstruct(G, B);
+            Ts_true = T * T;
+            [Gs, Bs] = toeplksquare(G, B, alg);
+            Ts = toeplkreconstruct(Gs, Bs);
+            testCase.assertEqual(Ts, Ts_true, 'AbsTol', n*eps);
+            
+        end
+    end
 end
 
-function test_singleton(TestCase)
-
-% 1x1 zero
-G = 0;
-B = 0;
-
-[Gs, Bs] = toeplksquare(G, B);
-T2 = toeplkreconstruct(Gs, Bs);
-TestCase.assertEqual(T2, 0);
-
-
-% 1x1 identity
-G = 1;
-B = 1;
-
-runallalgs(TestCase, 1, G, B)
-
-
-end
-
-function test_identity(TestCase)
-n=15;
-e1 = zeros(n,1);
-e1(1) = 1.0;
-[G, B] = toepgen(e1,e1);
-T2_true = eye(n);
-runallalgs(TestCase, T2_true, G, B, 'AbsTol', n * eps)
-
-
-end
-
-function test_random_real(TestCase)
-n = 12;
-c = randn(n,1);
-r = randn(n,1);
-r(1) = c(1);
-T = toeplitz(c,r);
-T2_true = T*T;
-
-[G, B] = toepgen(c,r);
-runallalgs(TestCase, T2_true, G, B, 'RelTol', 1024 * n * eps)
-end
-
-
-function test_random_complex(TestCase)
-n = 12;
-c = randn(n,1) + 1i*randn(n,1);
-r = randn(n,1) + 1i*randn(n,1);
-r(1) = c(1);
-T = toeplitz(c,r);
-T2_true = T*T;
-[G, B] = toepgen(c,r);
-runallalgs(TestCase, T2_true, G, B, 'RelTol', 128 * n * eps)
-
-end
-
-function runallalgs(testCase, Ts_true, G, B, varargin)
-
-for alg = {'full', 'fft'}
-    [Gs, Bs] = toeplksquare(G, B, alg{:});
-    Ts = toeplkreconstruct(Gs, Bs);
-    testCase.assertEqual(Ts, Ts_true, varargin{:});
-end
-
-end
 
