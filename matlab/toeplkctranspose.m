c9b8d872d13511b9b285f1f46fec64270d567e4c
Robert Luce <rbobluce@gmail.com>
2017-10-03 11:27:40 +0200

    Boilerplate commit for ctranspose

---
+++ matlab/toeplkctranspose.m
@@ -0,0 +1,6 @@
+function [G, B] = toeplkctranspose(G, B)
+% [G, B] = toeplkctranspose(G, B)  -- conjugate transepose
+%
+% Given a generator (G, B) of a TL matrix T, a generator for T' is
+% returned.
+end
