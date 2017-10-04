abd648104587da97676eaacb90547dcb74ca6528
Robert Luce <rbobluce@gmail.com>
2017-10-04 19:34:08 +0200

    Mark toepsolve as migrated

--- matlab/toepsolve.m
+++ matlab/toepsolve.m
@@ -2,6 +2,16 @@ function x = toepsolve(c, r, b)
 % x = toepsolve(c, r, b)
 %
 % Solve a Toeplitz system of equations in O(n^2) using drsolve package.
+%
+% Input:
+%
+%   c,r -- first column c and first row r of the Toeplitz matrix T
+%   b   -- RHS to solve for
+%
+% Output:
+%
+%   x   -- solution to Tx = b
+%
 
 n = length(c);
 
@@ -23,8 +33,8 @@ x = tsolve(c,r,b,1);
 
 % One step of iterative refinement.
 for ii=1:1
-res = toepmult(c,r,x) - b;
-xx = tsolve(c,r,res,1);
-x = x - xx;
+    res = toepmult(c,r,x) - b;
+    xx = tsolve(c,r,res,1);
+    x = x - xx;
 end
 end
