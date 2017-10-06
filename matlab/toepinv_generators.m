b04fddce76a74871390f84d67e27e6324060273d
Robert Luce <rbobluce@gmail.com>
2017-10-06 07:41:29 +0200

    Adapt for Sylvester displacement

--- matlab/toepinv_generators.m
+++ matlab/toepinv_generators.m
@@ -2,40 +2,42 @@ function [Ginv, Binv] = toepinv_generators(c, r)
 % [Ginv, Binv] = toepinv_generators(c, r)
 %
 % Computes a generator for the inverse of a Toeplitz matrix, with respect
-% to the Stein displacement equation T - Z*T*Z', with Z being the downshift
-% matrix.
+% to the Sylvester displacement equation
+%
+%   Zp * T - T * Zm,
+%
+% with Zp/m being the unit +/-1 circulants.
+%
+% Input:
+%   c,r -- first column/row of T
+%
+% Output:
+%   Ginv, Binv -- generator for T^{-1}
+%
+% Cost: Two solves with each of T and T', currently done via drsolve,
+% resulting in O(n^2).
+
+% We use the following approach:  Consider the augmentation
+%
+%   M = [
+%         -T, I;
+%          I, 0
+%       ]
+%
+% and the canoncial generator for T, G = [g, e1], B = [en, b].  It can be
+% checked that GG = [g, e1; 2*e1, 0] and BB = [en, b; 0, 2*en] is a
+% generator for M (w.r.t. Zp \oplus Zp / Zm \oplus Zm).  From the general
+% Schur complement formula the generator expressions below then follow.
 
 n = length(c);
 assert(length(r) == n && c(1) == r(1));
 
 % Match notation in the paper
-g = c(:);
-b = conj(r(:));
-b(1) = 0.;
+[G, B] = toepgen(c,r);
+e1 = G(:,2);
+en = B(:,1);
 
-e1 = zeros(n,1);
-e1(1) = 1.0;
-
-Ginv = solve_one(c,r, [-g, e1], false);
-Ginv(1,1) = Ginv(1,1) + 1;
-
-Binv = solve_one(c,r, [e1, -b], true);
-Binv(1,2) = Binv(1,2) + 1;
+Ginv = [ 2*e1, zeros(n,1) ] + toepsolve(c,r, -G);
+Binv = [ zeros(n,1), -2*en ] + toepsolve(conj(r), conj(c), B);
 
 end
-
-function x = solve_one(c, r, rhs, trans)
-% Compute (Z-I) * T(c,r)^{-1} * (I-Z)^{-1} * rhs
-% or the same system with T^* if 'trans' is true.
-
-x = -vapply(rhs, 'inv');
-if ~trans
-    x = -toepsolve(c,r,x);
-else
-    x = -toepsolve(conj(r),conj(c),x);
-end
-x = vapply(x);
-
-end
-
-
