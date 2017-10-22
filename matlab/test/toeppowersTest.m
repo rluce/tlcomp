d28de4acaa2298bb1a3b9ffe35577f8d171e35a5
Robert Luce <rbobluce@gmail.com>
2017-10-22 15:12:25 +0200

    Adapt for sylvester

--- matlab/test/toeppowersTest.m
+++ matlab/test/toeppowersTest.m
@@ -4,35 +4,58 @@ tests = functiontests(localfunctions);
 
 end
 
+function test_empty(testCase)
+% We require them to be empty, but don't require a specific size in the
+% nonzero dimension.
+
+c = [];
+r = [];
+T = [];
+[Gpowers, Bpowers] = toeppowers(c,r,101);
+
+for k = 1:101
+    testCase.assertTrue(isempty(Gpowers{k}));
+    testCase.assertTrue(isempty(Bpowers{k}));
+end
+
+end
+
+function test_zero(testCase)
+
+c = 0;
+r = 0;
+s = 4;
+
+[Gpowers, Bpowers] = toeppowers(c,r,s);
+check_generators(testCase, zeros(1), Gpowers, Bpowers);
+
+c = zeros(12,1);
+r = zeros(1,12);
+[Gpowers, Bpowers] = toeppowers(c,r,s);
+check_generators(testCase, zeros(12), Gpowers, Bpowers);
+end
+
 function test_singleton(testCase)
 c = 0.5;
 r = 0.5;
 s = 4;
 
-try_all_algs(testCase, c, r, s);
+T = 0.5;
+
+[Gpowers, Bpowers] = toeppowers(c, r, s);
+check_generators(testCase, T, Gpowers, Bpowers);
 
 end
 
-function test_reduced_size(testCase)
-% Check generator sizes of reduced algorithm
-n = 17;
-s = 5;
-[c,r] = random_toeplitz(n,n);
-[Gpowers, Bpowers] = toeppowers(c, r, s, 'reduced');
-
-for i=1:s
-    testCase.assertEqual(size(Gpowers{i}), [n,2*i]);
-    testCase.assertEqual(size(Bpowers{i}), [n,2*i]);
-end
-
-end
 
 function test_random(testCase)
-% Use canonical generators
-n=8;
+n = 8;
 s = 3;
 [c,r] = random_toeplitz(n,n);
-try_all_algs(testCase, c, r, s);
+
+T = toeplitz(c,r);
+[Gpowers, Bpowers] = toeppowers(c, r, s);
+check_generators(testCase, T, Gpowers, Bpowers);
 end
 
 
@@ -41,7 +64,11 @@ function test_prolate(testCase)
 n = 64;
 s = 13;
 T = -gallery('prolate', n, 0.4);
-try_all_algs(testCase, T(:,1), T(1,:), s);
+c = T(:,1);
+r = T(1,:);
+
+[Gpowers, Bpowers] = toeppowers(c, r, s);
+check_generators(testCase, T, Gpowers, Bpowers);
 end
 
 
@@ -52,8 +79,12 @@ s = 20;
 
 e1 = zeros(n,1);
 e1(1) = 1;
-try_all_algs(testCase, e1, e1, s);
+c = e1;
+r = e1;
 
+T = toeplitz(c,r);
+[Gpowers, Bpowers] = toeppowers(c, r, s);
+check_generators(testCase, T, Gpowers, Bpowers);
 
 end
 
@@ -62,39 +93,24 @@ function test_consistent_sizes(testCase)
 [c,r] = lps_example3(16);
 deg = 13;
 
-for alg = {'full', 'reduced'}
-    [Gp, Bp] = toeppowers(c, r, deg, alg{:});
-    for k=1:13
-        [~,s1] = size(Gp{k});
-        [~,s2] = size(Bp{k});
-        testCase.assertEqual(s1, s2);
-    end
-
+[Gp, Bp] = toeppowers(c, r, deg);
+for k=1:deg
+    [~,s1] = size(Gp{k});
+    [~,s2] = size(Bp{k});
+    testCase.assertEqual(s1, s2);
+    testCase.assertEqual(s1, 2*deg);
 end
 
 end
 
-function try_all_algs(testCase, c, r, s)
-
-T = toeplitz(c,r);
-
-[Gpowers, Bpowers] = toeppowers(c, r, s, 'full');
-check_generators(testCase, T, Gpowers, Bpowers);
-
-[Gpowers, Bpowers] = toeppowers(c, r, s, 'reduced');
-check_generators(testCase, T, Gpowers, Bpowers);
-
-
-end
-
-function check_generators(testCase, T, Gpowers, Bpowers)
-n = size(T,1);
+function check_generators(testCase, T, Gpowers, Bpowers, varargin)
 s = length(Bpowers);
 T_true  = eye(size(T));
+
 for i=1:s
     T_true = T_true * T;
     Tr = toeplkreconstruct(Gpowers{i}, Bpowers{i});
-    testCase.assertEqual(Tr, T_true, 'RelTol', 64*n*eps, 'AbsTol', 64*n*eps);
+    testCase.assertEqual(Tr, T_true, varargin{:});
 end
 
 
