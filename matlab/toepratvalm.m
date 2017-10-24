c9896323e1b39489d897be620a287d0713af48e4
Robert Luce <rbobluce@gmail.com>
2017-10-24 11:07:46 +0200

    Rational function eval

--- matlab/toepratvalm.m
+++ matlab/toepratvalm.m
@@ -13,10 +13,6 @@ function [G, B] = toepratvalm(c, r, p, q)
 % NOTE:  It is assumed that r(z) = p(z)/q(z) is proper (no common roots)
 % and that r(z) has no poles at or near the eigengalues of T.
 
-n = length(c);
-e1 = zeros(n,1);
-e1(1) = 1;
-
 % TODO
 % This is inefficient, as one single et of polynomials needs only to be
 % computed.
@@ -27,27 +23,6 @@ e1(1) = 1;
 [Gp, Bp] = gencompress(Gp, Bp);
 [Gq, Bq] = gencompress(Gq, Bq);
 
-% Formulas based on Theorem X.X (FIXME) for generators of r(T).
-G = [ratgen_solve(Gq, Bq, [-Gq, Gp]), e1];
-Bpart = ratgen_solve(Bq, Gq, [Bq, e1], Bp, Gp);
-B = [Bpart(:,1:(end-1)), Bp, Bpart(:,end)];
+[G, B] = toeplksolvetoeplk(Gq, Bq, Gp, Bp);
 
 end
-
-
-% Utiility function for rat generator construction
-function x = ratgen_solve(G, B, rhs, multG, multB)
-% solve TL system with G,B generated matrix and given rhs.  Optionally, do
-% an intermediate TL multiplication.
-
-x = vapply(rhs, 'inv');
-x = toeplksolve(G, B, x);
-if nargin >= 5
-    x = toeplkmult(multG, multB, x);
-end
-
-x = vapply(x);
-
-end
-
-
