4b6ab70aa25b10af4d7a2646e6a32bc0a94feac9
Robert Luce <rbobluce@gmail.com>
2017-10-26 22:33:10 +0200

    Make toepeye tests pass

--- matlab/toepeye.m
+++ matlab/toepeye.m
@@ -1,5 +1,35 @@
-function E = toepeye(n)
-% E = toepeye(n)  --  Identity matrix in TL format
+function E = toepeye(n, nbis)
+% E = toepeye(n)  --  Square identity matrix in ToepMat format
+%
+% See also: ToepMat.m
+if nargin == 1
+    if ~isscalar(n)
+        [mm,nn] = size(n);
+        if mm~= 1
+            error('tlcomp:InconsistentInput', 'Size vector must be row vector');
+        end
+        
+        if nn > 2
+            error('tlcomp:InconsistentInput', 'Size vector must have two elements');
+        end
+        
+        if n(1)~=n(2)
+            error('tlcomp:Unsupported', 'Only square matrices implemented');
+        end
+        
+        n = n(1);
+    end
+elseif nargin == 2
+    if ~isscalar(n) || ~isscalar(nbis)
+        error('tlcomp:InconsistentInput', 'dimensions must be scalars');
+    end
+    
+    if n ~= nbis
+        error('tlcomp:Unsupported', 'Only square matrices implemented');
+    end
+else
+    assert(false)
+end
 
 e1 = zeros(n,1);
 
