19e6b591b50dffc24d09f0b120d7d8cf07c10be1
Robert Luce <rbobluce@gmail.com>
2017-10-26 22:24:11 +0200

    Align syntax with Matlab eye

--- matlab/test/toepeyeTest.m
+++ matlab/test/toepeyeTest.m
@@ -10,11 +10,35 @@ E = toepeye(0);
 testCase.assertEqual(class(E), 'ToepMat');
 testCase.assertTrue(isempty(full(E)));
 
+E = toepeye([0,0]);
+testCase.assertEqual(class(E), 'ToepMat');
+testCase.assertTrue(isempty(full(E)));
+
+E = toepeye(0,0);
+testCase.assertEqual(class(E), 'ToepMat');
+testCase.assertTrue(isempty(full(E)));
+
 E = toepeye(1);
 testCase.assertEqual(class(E), 'ToepMat');
 testCase.assertEqual(full(E),1);
 
+E = toepeye(1,1);
+testCase.assertEqual(class(E), 'ToepMat');
+testCase.assertEqual(full(E),1);
+
+E = toepeye([1,1]);
+testCase.assertEqual(class(E), 'ToepMat');
+testCase.assertEqual(full(E),1);
+
 E = toepeye(9);
+testCase.assertEqual(class(E), 'ToepMat');
+testCase.assertEqual(full(E),eye(9));
+
+E = toepeye(9,9);
+testCase.assertEqual(class(E), 'ToepMat');
+testCase.assertEqual(full(E),eye(9));
+
+E = toepeye([9,9]);
 testCase.assertEqual(class(E), 'ToepMat');
 testCase.assertEqual(full(E),eye(9));
 
